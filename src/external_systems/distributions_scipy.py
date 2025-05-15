6c399e70ebf780399dab4abcece202c2998afbed
LeoCumpli21 <cumplileo@gmail.com>
2025-05-15 09:23:27 -0600

    removed unnecessary parameter loc

--- src/external_systems/distributions_scipy.py
+++ src/external_systems/distributions_scipy.py
@@ -15,14 +15,11 @@ class ExponentialDistributionScipy(DistributionModel):
     def fit(
         self,
         data: pd.DataFrame,
-        loc: float | None = None,
     ):
         """
         Fit the exponential distribution to the data.
-        If loc is specified, it will be used as a fixed parameter.
 
         :param data: The data to fit the model to.
-        :param loc: The location parameter of the distribution.
 
         :return: The fitted parameters of the distribution (loc, scale).
         """
