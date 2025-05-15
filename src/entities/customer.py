b6b8c77342071751472e24b28b7757d111778b32
LeoCumpli21 <cumplileo@gmail.com>
2025-05-15 09:25:12 -0600

    customer entity

---
+++ src/entities/customer.py
@@ -0,0 +1,5 @@
+class Customer:
+    def __init__(self, customer_id: int, arrival_time, ticket_type: str):
+        self.customer_id = customer_id
+        self.arrival_time = arrival_time
+        self.ticket_type = ticket_type
